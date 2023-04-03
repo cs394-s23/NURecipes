515607774e4f88f861aad584cd4b6429e413330d
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-04-03 18:49:27 -0500

    initialized card

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import PostCard from "./PostCard.jsx"
+import "./Homepage.css"
+
+const Homepage = () => {
+    return (
+        <div className="pageWrapper"> 
+            <PostCard/>
+        </div>
+    )
+}
+
+export default Homepage;
