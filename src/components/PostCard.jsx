3721d007bb899787e9b4f6e5fcd7d00761047f67
Eric Ma <ericma2024@u.northwestern.edu>
2023-04-03 20:32:03 -0500

    eric commit 2

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,15 +1,23 @@
 import "./PostCard.css";
+import React, { useState} from "react"
+
+
 // props: POST CLASS: title, caption, image, recipe, author...
 const PostCard = (props) => {
+
+    const [liked, setLiked] = useState(false);
+    const iconClass = liked? "liked" : "unliked"
+
     return (
         <div className="card">
     
             <div className="card-body">
-            <h5 className="card-user">{props.username}</h5>
+                <h5 className="card-user">{props.username}</h5>
                 <div className="card-image">
                     <img src={props.image} className = "foodImg" alt="" />
                 </div>
-                <img src="https://e7.pngegg.com/pngimages/799/396/png-clipart-heart-gold-heart-icon-love-metal.png" className = "likeImg"/>
+                <button className={iconClass}><img className='icon-img' src="src/assets/emptyheart.png" 
+                    alt="my image" onClick={() => {setLiked(!liked)}} /></button>               
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.caption}</p>
                 <a href="#" className="btn btn-primary">View Recipe</a>
