338ab55bf157538d31269cc423994c8ce81938f6
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-04-03 19:13:34 -0500

    Eric

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,9 +3,9 @@ import "./Homepage.css"
 
 const Homepage = () => {
     return (
-        <div className="pageWrapper"> 
-            <PostCard/>
-        </div>
+        <PostCard  title={"test-title"} image={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'} username={'user'}
+            numLikes = {5} dateTime ={"1-1-01"} cost = {10} timeToMake = {60} caption={'lorem ipsum dolor sit'}/>
+
     )
 }
 
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,13 +1,16 @@
-
+import "./PostCard.css";
 // props: POST CLASS: title, caption, image, recipe, author...
 const PostCard = (props) => {
     return (
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
     
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{props.title}</h5>
+                <div className="card-image">
+                    <img src={props.image} alt="" />
+                </div>
+                <p className="card-text">{props.caption}</p>
+                <a href="#" className="btn btn-primary">View Recipe</a>
             </div>
         </div>
     )
