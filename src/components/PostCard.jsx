130132331b9d14b2898c95b93eb3717c54097369
rbyat <rushilbyatnal2024@u.northwestern.edu>
2023-04-03 20:05:39 -0500

    Rushil

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,6 +9,7 @@ const PostCard = (props) => {
                 <div className="card-image">
                     <img src={props.image} className = "foodImg" alt="" />
                 </div>
+                <img src="https://e7.pngegg.com/pngimages/799/396/png-clipart-heart-gold-heart-icon-love-metal.png" className = "likeImg"/>
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.caption}</p>
                 <a href="#" className="btn btn-primary">View Recipe</a>
