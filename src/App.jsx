b3d0b40b27c4d9a167a0c3f97bdcdd3f6831e80e
Alex Tang <alexandertang2024@u.northwestern.edu>
2023-04-03 20:29:24 -0500

    database initialized

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useDbData } from './utilities/firebase';
 
 import Homepage from './components/Homepage.jsx'
 
-function App() {
+const App = () => {
+  const [data, error] = useDbData('/')
 
   return (
-    <Homepage/>
+    <Homepage data={data}/>
   )
 }
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,16 @@
 import PostCard from "./PostCard.jsx"
 import "./Homepage.css"
 
-const Homepage = () => {
+const Homepage = ({data}) => {
+    if (!data) {
+        return <h1>Data is loading...</h1>;
+    }
     return (
-        <PostCard  title={"test-title"} image={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'} username={'user'}
+        <div>
+            <PostCard  title={"test-title"} image={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'} username={'user'}
             numLikes = {5} dateTime ={"1-1-01"} cost = {10} timeToMake = {60} caption={'lorem ipsum dolor sit'}/>
+        </div>
+        
 
     )
 }
