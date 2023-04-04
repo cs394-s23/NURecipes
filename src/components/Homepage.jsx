0831840b4488ee6afeef5a58c80d6307fe11c19b
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-04-03 22:25:12 -0500

    css changes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,8 +7,8 @@ const Homepage = ({data}) => {
     }
     return (
         <div>
-            <PostCard  title={"test-title"} image={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'} username={'user'}
-            numLikes = {5} dateTime ={"1-1-01"} cost = {10} timeToMake = {60} caption={'lorem ipsum dolor sit'}/>
+            <PostCard  title={"Really good pizza"} image={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'} username={'Troy Barnes'}
+            numLikes = {5} dateTime ={"1-1-01"} cost = {10} timeToMake = {60} caption={'Wow this pizza is so good. I wish I could eat it every day for the rest of my life. In fact, I wish this was all anyone every ate for the rest of time. No other foods should be allowed, and if you don\'t like it, that\'s on you. If you\'re allergic to something, maybe we can make an exception.'} profilePic={"https://openpsychometrics.org/tests/characters/test-resources/pics/C/4.jpg"} />
         </div>
         
 
