2feef445dde3820f19dc511b890585a3a6fee1cf
Lena Sauter <lenasauter2023@u.northwestern.edu>
2023-04-09 17:19:20 -0500

    lena april 9

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,12 @@
 import PostCard from "./PostCard.jsx"
 
-
-
-
 const Feed = ({data}) => {
     console.log('feed data', data)
     if (!data) {
         return <h1>Data is loading...</h1>;
     }
     return (
-        <div>
+        <div className = "feed-container">
             {data.map((post, index) => <PostCard props={post} key={index} />)}
         </div>
         );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import PostCard from "./PostCard.jsx"
 import Feed from "./Feed.jsx"
 import "./NavBar.css"
+import {Navbar} from "react-bootstrap"
 
 const NavBar = () => {
     return (
@@ -16,7 +17,7 @@ const NavBar = () => {
                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
+                    <a className="nav-link" href="/discover">Discover</a>
                 </li>
                 <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
