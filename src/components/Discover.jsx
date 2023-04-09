4b5fbf952f0a3f9a88d6b4189e24c4bb7c030131
Alex Tang <alexandertang2024@u.northwestern.edu>
2023-04-09 18:26:54 -0500

    alex add discover april 9

---
+++ src/components/Discover.jsx
@@ -0,0 +1,31 @@
+import PostCard from "./PostCard.jsx"
+import Feed from "./Feed.jsx"
+import "./Homepage.css"
+import React, { useState, useEffect } from "react"
+
+const Discover = ({data}) => {
+
+    console.log("unsorted data: ", data);
+    
+    if (!data) {
+        return <h1>Data is loading...</h1>;
+    }
+
+    const sortedData = data.sort((a,b) => {
+        return a.like_count > b.like_count ? -1 : 1
+    })
+
+    console.log("post-sort data: ", data);
+    console.log("sorted data: ", sortedData);
+
+    return (
+        <div className = "feed">
+            <div className = "leaderboard">
+                <h1 style={{color:"white"}}> This week's leaders are: </h1>
+            </div>
+            < Feed data = {sortedData}  />
+        </div>
+    )
+}
+
+export default Discover;
