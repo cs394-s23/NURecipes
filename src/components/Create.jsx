8d302695b6a5d97b3e1b9109d37238a7e24f2278
Eric Ma <ericma2024@u.northwestern.edu>
2023-04-18 19:57:15 -0500

    eric apr 18 v2

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -96,115 +96,115 @@ const Create = ({ data }) => {
 
           pushDb(test, "Recipes/");
 
-          navigate(`/#`)
+          navigate(`/#`);
         });
       }
     );
-
- 
   };
 
   //need title, picture, ingredients, steps, cost, time
   return (
-    <Form className="create-form" onSubmit={onFormSubmit}>
-      <Form.Group className="mb-3" controlId="formTitle">
-        <Form.Label>Title</Form.Label>
-        <Form.Control placeholder="Enter Title" name="title" />
-      </Form.Group>
+    <div>
+      <Form className="create-form" onSubmit={onFormSubmit}>
+        <Form.Group className="mb-3" controlId="formTitle">
+          <Form.Label>Title</Form.Label>
+          <Form.Control placeholder="ex: Alex's pizza" name="title" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formIngredients">
-        <Form.Label>Ingredients</Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Ingredients (Separate by commas)"
-          rows={2}
-          name="ingredients"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formSteps">
-        <Form.Label>Steps</Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Steps (Separate by commas)"
-          rows={2}
-          name="steps"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formCaption">
-        <Form.Label>Details</Form.Label>
-        <Form.Control
-          as="textarea"
-          placeholder="Caption"
-          rows={2}
-          name="caption"
-        />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formIngredients">
+          <Form.Label>Ingredients (Separate by <strong>commas</strong>)</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="ex: dough, cheese, marinara sauce"
+            rows={2}
+            name="ingredients"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formSteps">
+          <Form.Label>Steps (Separate by <strong>commas</strong>)</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="ex: put sauce on dough,  place cheese over, bake the pizza"
+            rows={2}
+            name="steps"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formCaption">
+          <Form.Label>Details</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="ex: it was yummy!"
+            rows={2}
+            name="caption"
+          />
+        </Form.Group>
 
-      <Form.Group controlId="mb-3" className="mb-3">
-        <Form.Label>Tags</Form.Label>
-        <div className="tags-section">
-          <ToggleButton
-            className="tag-button"
-            variant="outline-success"
-            type="checkbox"
-            checked={veggie}
-            onClick={() => handleVeggie()}
-          >
-            {" "}
-            Vegetarian{" "}
-          </ToggleButton>
-          <ToggleButton
-            className="tag-button"
-            variant="outline-success"
-            type="checkbox"
-            checked={vegan}
-            onClick={() => handleVegan()}
-          >
-            {" "}
-            Vegan{" "}
-          </ToggleButton>
-          <ToggleButton
-            className="tag-button"
-            variant="outline-success"
-            type="checkbox"
-            checked={glutenFree}
-            onClick={() => handleGlutenFree()}
-          >
-            {" "}
-            Gluten-Free{" "}
-          </ToggleButton>
-          <ToggleButton
-            className="tag-button"
-            variant="outline-success"
-            type="checkbox"
-            checked={dairyFree}
-            onClick={() => handleDairyFree()}
-          >
-            {" "}
-            Dairy-Free{" "}
-          </ToggleButton>
-        </div>
-        {/* <Form.Control type="file" name="image_url"/> */}
-      </Form.Group>
-      <InputGroup className="mb-3">
-        <InputGroup.Text>Cost and Time</InputGroup.Text>
-        <Form.Control aria-label="Cost" placeholder="Cost" name="cost" />
-        <Form.Control
-          aria-label="Time"
-          placeholder="Minutes"
-          name="cook_time"
-        />
-      </InputGroup>
+        <Form.Group controlId="mb-3" className="mb-3">
+          <Form.Label>Tags</Form.Label>
+          <div className="tags-section">
+            <ToggleButton
+              className="tag-button"
+              variant="outline-success"
+              type="checkbox"
+              checked={veggie}
+              onClick={() => handleVeggie()}
+            >
+              {" "}
+              Vegetarian{" "}
+            </ToggleButton>
+            <ToggleButton
+              className="tag-button"
+              variant="outline-success"
+              type="checkbox"
+              checked={vegan}
+              onClick={() => handleVegan()}
+            >
+              {" "}
+              Vegan{" "}
+            </ToggleButton>
+            <ToggleButton
+              className="tag-button"
+              variant="outline-success"
+              type="checkbox"
+              checked={glutenFree}
+              onClick={() => handleGlutenFree()}
+            >
+              {" "}
+              Gluten-Free{" "}
+            </ToggleButton>
+            <ToggleButton
+              className="tag-button"
+              variant="outline-success"
+              type="checkbox"
+              checked={dairyFree}
+              onClick={() => handleDairyFree()}
+            >
+              {" "}
+              Dairy-Free{" "}
+            </ToggleButton>
+          </div>
+          {/* <Form.Control type="file" name="image_url"/> */}
+        </Form.Group>
+        <InputGroup className="mb-3">
+          <InputGroup.Text>Cost and Time</InputGroup.Text>
+          <Form.Control aria-label="Cost" placeholder="Cost" name="cost" />
+          <Form.Control
+            aria-label="Time"
+            placeholder="Minutes"
+            name="cook_time"
+          />
+        </InputGroup>
 
-      <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>Picture</Form.Label>
-        <Form.Control type="file" onChange={handleChange} name="image_url" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      {/* <p>{percent} % done</p> */}
-    </Form>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Picture</Form.Label>
+          <Form.Control type="file" onChange={handleChange} name="image_url" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        {/* <p>{percent} % done</p> */}
+      </Form>
+    </div>
   );
 };
 
